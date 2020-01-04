0e8f257e5fb03fcf4e3ec1f5a60e388e35d712e7
klaus126 <klaus@llaabb.kr>
2020-01-04 22:12:27 +0900

    bulls-eye-project

---
+++ bulls-eye/bulls-eye/bulls-eye/ContentView.swift
@@ -0,0 +1,90 @@
+//
+//  ContentView.swift
+//  bulls-eye
+//
+//  Created by Mun Jonghae on 2020/01/04.
+//  Copyright © 2020 Mun Jonghae. All rights reserved.
+//
+
+
+import SwiftUI
+
+struct ContentView: View {
+    @State var whosThereIsVisible: Bool = false
+    @State var sliderValue: Double = 50.0
+    
+    var body: some View {
+        VStack {
+            Spacer()
+            // Target row
+            HStack {
+                Text("Put the bullseye as close as you can to: ")
+                Text("100")
+            }
+            Spacer()
+            
+            // Slider row
+            HStack {
+                Text("0")
+                Slider(value: self.$sliderValue, in: 1...100)
+                Text("100")
+            }
+            Spacer()
+            
+            Button(action: {
+                self.whosThereIsVisible = true
+            }) {
+                Text("Hit Me!")
+            }
+            .alert(isPresented: $whosThereIsVisible) {() -> Alert in
+                var roundedValue: Int = Int(self.sliderValue.rounded())
+                return Alert(title: Text("Hello there"),
+                      message: Text("The slider value is \(roundedValue)"),
+                      dismissButton: .default(Text("Awesome"))
+            )}
+            
+           
+
+            
+            Spacer()
+            // Score row
+            HStack {
+                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                    Text("Start Over")
+                        .multilineTextAlignment(.leading)
+                }
+                /*.alert(item: $alertIsVisible, content: <#T##(Identifiable) -> Alert#>)
+                */
+                Spacer()
+                Text("Score: ")
+                Text("999999")
+                
+                Spacer()
+                Text("Round: ")
+                Text("999")
+                
+                Spacer()
+                Button(action: {}) {
+                    Text("info")
+                        .multilineTextAlignment(.trailing)
+                }
+            }
+            .padding(.bottom, 20) //.padding(from where, distance)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView().previewLayout(.fixed(width:896, height:414))
+    }
+}
+
+
+
+
+
+/*
+ 
+ }
+ */
